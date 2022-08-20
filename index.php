d34234b54546dcfd417cbece96c2885096efce9f
NastyaLush <yfcn.if1203@gmail.com>
2022-08-20 15:09:16 +0300

    with update mistake

--- index.php
+++ index.php
@@ -11,9 +11,9 @@ session_start();
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title>lab-work 1</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="main.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap" rel="stylesheet">
 <body>
 
