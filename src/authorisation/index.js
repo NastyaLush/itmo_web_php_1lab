8c552b9af90a972dbdda446761f8e3f165469d0b
NastyaLush <yfcn.if1203@gmail.com>
2022-09-30 22:17:07 +0300

    f

--- src/authorisation/index.js
+++ src/authorisation/index.js
@@ -23,8 +23,8 @@ firebase.initializeApp(firebaseConfig);
 
 const ui = new firebaseui.auth.AuthUI(firebase.auth())
 ui.start('#firebaseui-auth-container', {
-  signInSuccessUrl: '/lab_web1/src/main/index.html',
-  // signInSuccessUrl: 'https://se.ifmo.ru/~s336767/src/main/index.html',
+  //signInSuccessUrl: '/lab_web1/src/main/index.html',
+  signInSuccessUrl: 'https://se.ifmo.ru/~s336767/src/main/index.html',
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
